9ffde2427f68a6035397d9af78b94856b09117d8
Hilario Medina <johimesa@gmail.com>
2020-02-05 22:52:46 -0500

    Se crea el proyecto

---
+++ udemy/src/main/java/pe/com/johimesa/udemy/App.java
@@ -0,0 +1,13 @@
+package pe.com.johimesa.udemy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
