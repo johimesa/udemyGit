9deab4af080dcdb34c27129ed6685d903ab2485e
José Hilario <44994863+johimesa@users.noreply.github.com>
2020-02-06 22:01:49 -0500

    se quita el comentario antes del sdf

--- udemy/src/main/java/pe/com/johimesa/udemy/util/Utilitario.java
+++ udemy/src/main/java/pe/com/johimesa/udemy/util/Utilitario.java
@@ -6,9 +6,7 @@ import java.util.Date;
 public class Utilitario {
 
 	public static String formatDate(Date fecha, String formato) {
-		
-		// algo comentado
-		
+				
 		SimpleDateFormat sdf = new SimpleDateFormat(formato);
 		
 		return sdf.format(fecha);
