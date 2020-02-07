cf55022509e3d5a6551231db59452b4538dd9b53
Hilario Medina <johimesa@gmail.com>
2020-02-06 22:44:59 -0500

    Se agrega los set y get

--- udemy/src/main/java/pe/com/johimesa/udemy/util/Libro.java
+++ udemy/src/main/java/pe/com/johimesa/udemy/util/Libro.java
@@ -4,4 +4,17 @@ public class Libro {
 
 	private String color;
 	private String autor;
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
 }
