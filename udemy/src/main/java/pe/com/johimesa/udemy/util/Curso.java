fec3fb5e09ede7f372a7a25931d639fbf6cb2c7f
Hilario Medina <johimesa@gmail.com>
2020-02-09 17:04:49 -0500

    Se agrega más atributos en master

--- udemy/src/main/java/pe/com/johimesa/udemy/util/Curso.java
+++ udemy/src/main/java/pe/com/johimesa/udemy/util/Curso.java
@@ -3,5 +3,12 @@ package pe.com.johimesa.udemy.util;
 public class Curso {
 
 	private String nombre;
-	private String especialidad;
+	private String especialidad;// atrib
+	private boolean stock;
+	
+	private void pintar() {
+		System.out.println("nombre: " + nombre);
+		System.out.println("especialidad: " + especialidad);
+		System.out.println("stock: " + stock);
+	}
 }
